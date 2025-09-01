fec5999e8b922a1fd30e9c093b8f9c7ccff0e4e5
viniciusoliveirabittencourt <viniciusbittencourt2003@gmail.com>
2025-09-01 15:09:55 -0300

    Testando links

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <main>
       <div>Teste Topado</div>
+      <a href="./tierList/menu">Link</a>
     </main>
   );
 }
---
+++ app/tierList/menu/page.tsx
@@ -0,0 +1,8 @@
+export default function Menu() {
+    return (
+        <>
+            <h1>Está funcionando</h1>
+            <a href="../../">Link</a>
+        </>
+    )
+}
